55bda654b5a02da12f27ea7b7f3315aa322d4ef3
itsorivera <its.orivera@gmail.com>
2026-03-16 23:56:04 -0500

    feat: adding general agent prompt

---
+++ src/core/prompts.py
@@ -0,0 +1,7 @@
+from jinja2 import Template
+
+GENERAL_AGENT_PROMPT = Template(
+"""
+You are an intelligent assistant designed to help users solve complex problems using a variety of tools.
+Your goal is to provide accurate and helpful responses, effectively utilizing the tools at your disposal.
+""")
